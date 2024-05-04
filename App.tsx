66fb125ff9251dfc27dfc74474d8e4e5e8242f21
Antonio Nitoi <64855012+4N70N1U5@users.noreply.github.com>
2024-05-04 20:39:08 +0300

    Fix Status Bar style

--- App.tsx
+++ App.tsx
@@ -4,22 +4,9 @@ import Router from './src/router/Router';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Login />
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <AuthContextProvider>
+      <StatusBar style="auto" />
       <Router />
     </AuthContextProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
